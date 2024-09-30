96ee1c7b45406528842bd25c804ee6ab4c85a3d1
michaellgans <michaellgans@Victoria>
2024-09-30 13:24:15 -0500

    MLG - 1:24 PM - updates dynamic page

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,3 +1,6 @@
-export default function Page({ params }: { params: { id: string } }) {
-    return <div>Topic Page: {params.id}</div>;
-  }
+export default async function Page({ params }: { params: { id: string } }) {
+  //Simulate Page loading
+  await new Promise((r) => setTimeout(r, 3000));
+
+  return <div>Topic Page: {params.id}</div>;
+}
