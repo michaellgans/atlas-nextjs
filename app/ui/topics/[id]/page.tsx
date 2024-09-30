5bfa3ac9edd51c94d19b08d67701ebce234f9933
michaellgans <michaellgans@Victoria>
2024-09-30 13:48:02 -0500

    MLG - 1:47 PM - changes wait

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,6 +1,6 @@
 export default async function Page({ params }: { params: { id: string } }) {
   //Simulate Page loading
-  await new Promise((r) => setTimeout(r, 3000));
+  await new Promise((r) => setTimeout(r, 500));
 
   return <div>Topic Page: {params.id}</div>;
 }
