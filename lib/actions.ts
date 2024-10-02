22d58e6e6c50df5dc63c5dfe01b8f8ade781bc61
michaellgans <michaellgans@Victoria>
2024-10-02 13:40:42 -0500

    MLG - 1:40 PM - corrects input for AskQuestion form

--- lib/actions.ts
+++ lib/actions.ts
@@ -25,18 +25,18 @@ export async function addTopic(data: FormData) {
 }
 
 export async function addQuestion(question: FormData) {
-    try {
-      insertQuestion({
-        title: question.get("title") as string,
-        topic_id: question.get("topic_id") as string,
-        votes: 0,
-      });
-      revalidatePath("/ui/topics/[id]", "page");
-    } catch (error) {
-      console.error("Database Error:", error);
-      throw new Error("Failed to add question.");
-    }
+  try {
+    await insertQuestion({
+      title: question.get("title") as string,
+      topic_id: question.get("topic_id") as string,
+      votes: 0,
+    });
+    revalidatePath("/ui/topics/[id]", "page");
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to add question.");
   }
+}
 
 export async function addVote(data: FormData) {
     try {
